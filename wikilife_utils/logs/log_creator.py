69c9ea61d2b75c8a79abed6617e86dbda2e61e2f
Emmanuel <ecarelli@wikilife.com>
2014-04-30 15:40:56 -0300

    date type check

--- wikilife_utils/logs/log_creator.py
+++ wikilife_utils/logs/log_creator.py
@@ -1,5 +1,8 @@
 # coding=utf-8
 
+from wikilife_utils.formatters.date_formatter import DateFormatter
+
+
 class LogCreator(object):
     """
     Raw logs
@@ -15,6 +18,13 @@ class LogCreator(object):
 
     @staticmethod
     def create_log(user_id, start, end, text, source, nodes):
+
+        if not isinstance(start, str):
+            start = DateFormatter.to_datetime(start)
+
+        if end and not isinstance(end, str):
+            end = DateFormatter.to_datetime(end)
+
         return {
             "id": 0,
             "userId": user_id,
